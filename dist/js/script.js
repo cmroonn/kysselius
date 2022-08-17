993fb421ebe7ddcb1432edc4149c685fd4d96766
cmroonn <fallgetell@gmail.com>
2022-08-17 12:42:15 +0500

    upd

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,52 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (window.innerWidth > 992) {
-    // Animation settings
-    var scrollController = new ScrollMagic.Controller();
-    var uniqueScene = new ScrollMagic.Scene({
-      triggerElement: '.unique-decor',
-      duration: 520,
-      offset: -300
-    }).setPin('.unique-decor').addTo(scrollController);
-    var servicesScene = new ScrollMagic.Scene({
-      triggerElement: '.welcome__title',
-      duration: 500,
-      offset: 250
-    }).setPin('.services-decor').addTo(scrollController);
-    var contactScene = new ScrollMagic.Scene({
-      triggerElement: '.unique-decor',
-      duration: 600,
-      offset: 0
-    }).setPin('.contact-decor').addTo(scrollController); // const welcomeDot = new ScrollMagic.Scene({triggerElement: '.main'})
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".services-decor", {
+    scrollTrigger: {
+      trigger: ".services-decor",
+      scrub: 3
+    },
+    top: 300,
+    ease: "linear",
+    duration: 10
+  });
+  gsap.to(".unique-decor", {
+    scrollTrigger: {
+      trigger: ".unique-decor",
+      scrub: 7,
+      end: '+=500'
+    },
+    top: 300,
+    ease: "linear",
+    duration: 3
+  });
+  gsap.to(".contact-decor", {
+    scrollTrigger: {
+      trigger: ".contact-decor",
+      scrub: 5,
+      end: 'bottom'
+    },
+    top: 100,
+    ease: "linear",
+    duration: 3
+  });
+
+  if (window.innerWidth > 992) {// Animation settings
+    // const scrollController = new ScrollMagic.Controller();
+    //
+    // const uniqueScene = new ScrollMagic.Scene({triggerElement: '.unique-decor', duration: 520, offset: -300})
+    //     .setPin('.unique-decor')
+    //     .addTo(scrollController)
+    //
+    // const servicesScene = new ScrollMagic.Scene({triggerElement: '.welcome__title', duration: 500, offset: 250})
+    //     .setPin('.services-decor')
+    //     .addTo(scrollController)
+    //
+    // const contactScene = new ScrollMagic.Scene({triggerElement: '.unique-decor', duration: 600, offset: 0})
+    //     .setPin('.contact-decor')
+    //     .addTo(scrollController)
+    // const welcomeDot = new ScrollMagic.Scene({triggerElement: '.main'})
     //     .setVelocity('#title_dot', {rotate: "360deg"}, {duration: 1000, easing: 'linear'})
     //     .addTo(scrollController);
   } // scroll to bottom
